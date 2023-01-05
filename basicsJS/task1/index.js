1b94f4b1e6cd6485514062bb1819751b6841b011
dzoric1 <Gerasimov2099@mail.ru>
2023-01-05 14:25:47 +0800

    realized task 1

--- basicsJS/task1/index.js
+++ basicsJS/task1/index.js
@@ -19,3 +19,17 @@
 
 "use strict";
 
+let numberOfFilms = Number(prompt('Сколько фильмов вы уже посмотрели', ''));
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+personalMovieDB.movies[prompt('Один из последних просмотренных фильмов?')] = prompt('На сколько оцените его?');
+personalMovieDB.movies[prompt('Один из последних просмотренных фильмов?')] = prompt('На сколько оцените его?');
+
+console.log(personalMovieDB);
