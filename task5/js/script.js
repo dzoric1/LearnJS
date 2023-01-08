57e3ac7eadbcd519311a959bbb29c016ad3a8964
dzoric1 <Gerasimov2099@mail.ru>
2023-01-08 14:04:27 +0800

    realized task 5

---
+++ task5/js/script.js
@@ -0,0 +1,51 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+  movies: [
+    "Логан",
+    "Лига справедливости",
+    "Ла-ла лэнд",
+    "Одержимость",
+    "Скотт Пилигрим против...",
+  ]
+};
+
+const adv = document.querySelectorAll('.promo__adv img');
+const promoBg = document.querySelector('.promo__bg');
+const promoGenre = promoBg.querySelector('.promo__genre');
+const promoInteractiveList = document.querySelector('.promo__interactive-list');
+const promoInteractiveItems = promoInteractiveList.querySelectorAll('.promo__interactive-item');
+
+adv.forEach(item => {
+  item.remove();
+});
+
+promoGenre.textContent = 'Драма';
+
+promoBg.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieDB.movies.sort();
+
+promoInteractiveList.innerHTML = '';
+
+movieDB.movies.forEach((film, i) => {
+  promoInteractiveList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1} - ${film}
+      <div class="delete"></div>
+    </li>`;
+});
+
